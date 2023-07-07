ea13b945ee443245cf3300aa2a632b787fc38b6d
zoselaslwl <smj980902@gmail.com>
2023-07-07 17:34:11 +0900

    tpj

--- vuetpj/src/views/HomeView.vue
+++ vuetpj/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>병 민이 의 프로젝트</h1>
+    <h1>병민이조 의 프로젝트</h1>
     <div class="image-container">
       <img
         src="https://post-phinf.pstatic.net/MjAxOTExMDhfOTgg/MDAxNTczMjAwMDkyNzIz.mMIFIyZ6uQ8JnquecBDjn40hC3ZxcP8LrQBaPrjHEaMg.NznpkpdZUViYMBB22Z38kakbtVb459kzVZcIySEiG4gg.GIF/IuwTcMm7SZ7BLkPibmyNLLy_mIuQ.jpg?type=w400"
